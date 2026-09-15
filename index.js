8136b1f81849c8f093b4fcc8be3f905ae9f95f87
s-mumin <93403897+s-mumin@users.noreply.github.com>
2026-09-16 00:02:12 +0100

    Update alert message and add mobile menu functionality

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// alert("Coming Soon!")
+alert("More Update To The Site. Stay Tuned!")
+
+
+var menuBtn = document.querySelector("#navbar-menu-button")
+var menuIcon = document.querySelector(".fa-bars")
+var mobileMenu = document.querySelector('.navbar-links-mobile')
+var navbar = document.getElementById('navbar')
+
+
+menuBtn.addEventListener('click', () => {
+
+
+    mobileMenu.classList.toggle('navbar-links-mobile-active')
+
+    menuIcon.classList.toggle('fa-x')
+
+    navbar.classList.toggle('dark')
+})
+
+        var dateobj = new Date();
+        var dateObject = dateobj.getFullYear();
+
+var copyright = document.getElementById('copyright').innerHTML = "" + dateObject;
